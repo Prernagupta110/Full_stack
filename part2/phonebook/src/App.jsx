708a112067ec12f3d732e8b21554279875f44f45
Prerna Gupta <49404478+Prernagupta110@users.noreply.github.com>
2024-07-26 22:15:19 +0300

    Update App.jsx

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import personService from './components/person';
+import personService from './services/person';
 
 const Filter = ({ newFilterName, setNewFilterName }) => {
   const handleFilterNameChange =
